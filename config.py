e8b7b0f8658f9f10527818f4770aa8ceb9e1664a
r_junior8 <r_junior@protonmail.com>
2019-03-22 03:16:49 +0000

    Initial commit

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 DEBUG = True
 
 # In Arch Linux
-SQLALCHEMY_DATABASE_URI = "mysql://neonumy:init88@localhost/neonumy_db"
+SQLALCHEMY_DATABASE_URI = "mysql://user:password@localhost/database"
 
 # In Ubuntu
 #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost/database"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SECRET_KEY = "71@!NeOnUMy!#58!!nEoNuMy*&87"
+SECRET_KEY = "super_secret"
